759258e3db777aeaca98d82fa893c4bb892eafdd
Alex <alex0klain@gmail.com>
2017-07-12 20:53:56 +0300

    Update .2
    * Добавлена прокрутка страницы
    * Удалены лишний файлы

--- js/load.js
+++
@@ -1,8 +0,0 @@
-$(function() {
-	var link = $('a[data-toggle="modal"]');
-	link.on('click', function(event) {
-		var work = $(this).attr('data-work');
-		var modal = $('.modal');
-		modal.find('.modal-body').load(work+'.html');
-	});
-});
